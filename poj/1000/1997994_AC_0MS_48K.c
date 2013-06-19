4e907bbad640776f9c5e0b0813c1a72acfb25229
wangyongliang.wyl@gmail.com <wangyongliang.wyl@gmail.com>
2013-06-19 14:31:53 +0800

    remove duplicate files

--- poj/1000/1997994_AC_0MS_48K.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main()
-{
-	int a,b;
-	while(EOF!=scanf("%d %d",&a,&b))
-	{
-		printf("%d",a+b);
-	}
-}
--- poj/3062/3257706_AC_0MS_276K.cc
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-char s[100];
-while(gets(s))
-{
-puts(s);
-}
-}
