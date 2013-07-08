209261f8c6296d4ec045f8e73a06bdde2909a5e1
wangyongliang.wyl@gmail.com <wangyongliang.wyl@gmail.com>
2013-07-08 16:30:24 +0800

    Add solution of problem A for world finals 2013

--- acm-icpc/2013/world_final/A.cc
+++ acm-icpc/2013/world_final/A.cc
@@ -9,7 +9,7 @@
 #include <cstdio>
 #include <cstdlib>
 using namespace std;
-
+ 
 #define maxn 40100
 vector<int> types[maxn];
 int main() {
@@ -17,7 +17,7 @@ int main() {
   scanf("%d", &n);
   bool a[53][53] = {0};
   char str[10];
-
+ 
   for (int i = 0; i < n; i ++) {
     scanf("%s", str);
     types[i].clear();
@@ -25,35 +25,39 @@ int main() {
       if (str[j] != '0') {
         types[i].push_back(int(str[j] - 'A' + 26 * (str[j + 1] == '+')));
       }
-
+ 
     }
     for (int j = 0; j < types[i].size(); j ++) {
       for (int k = 0; k < types[i].size(); k ++) {
-        if (types[i][j] != types[i][k]) {
+        // if (types[i][j] != types[i][k]) {
+        if (j != k) {
           a[types[i][k]][types[i][j]] = a[types[i][j]][types[i][k]] = 1;
         }
       }
     }
   }
-
+ 
   for (int k = 0; k < 26; k ++) {
     for (int i = 0; i < 52; i ++) {
       for (int j = 0; j < 52; j ++) {
         if (a[i][k] && a[k + 26][j]) {
           a[i][j] = 1;
         }
+        if (a[i][k + 26] && a[k][j])
+          a[i][j] = 1;
       }
     }
   }
   bool result = false;
   int cnt = 0;
   for (int i = 0; i < 26; i ++) {
-    if (a[i][i + 26] || a[i][i] || a[i + 26][i + 26]) {
+    if (a[i][i + 26] || a[i + 26][i]) {
       // cout << i << endl;
       result = true;
+      break;
     }
   }
-
+ 
   cout << (result? "unbounded": "bounded") << endl;
   return 0;
 }
